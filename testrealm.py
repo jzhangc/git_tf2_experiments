13796b8888b69ad098427efed639b7cda509f0d3
Jing Zhang <jzhangc@gmail.com>
2019-12-18 18:37:16 -0500

    initial commit

--- testrealm.py
+++ testrealm.py
@@ -9,7 +9,6 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-
 # ------ setup working dictory ------
 main_dir = os.path.abspath('./')
 dat_dir = os.path.join(main_dir, '0.data')
