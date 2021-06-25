34e872ab5a09a7cb5991fdd3f412863ebb5adad1
Jing Zhang <jzhangc@gmail.com>
2021-06-25 10:50:40 -0400

    routine updates

--- testrealm_dataloader_singlefile.py
+++ testrealm_dataloader_singlefile.py
@@ -347,171 +347,171 @@ if args.model_type == 'classification':
 
 
 # ------ loacl classes ------
-# class DataLoader(object):
-#     """
-#     # Purpose
-#         Data loading class.
-#     # Methods
-#         __init__: load data and other information from argparser, as well as class label encoding for classification study
-#     # Details
-#         This class is designed to load the data and set up data for training LSTM models.
-#         This class uses the custom error() function. So be sure to load it.
-#     # Class property
-#         modelling_data: dict. data for model training. data is split if necessary.
-#             No data splitting for the "CV only" mode.
-#             returns a dict object with 'training' and 'test' items
-#     """
+class DataLoader(object):
+    """
+    # Purpose
+        Data loading class.
+    # Methods
+        __init__: load data and other information from argparser, as well as class label encoding for classification study
+    # Details
+        This class is designed to load the data and set up data for training LSTM models.
+        This class uses the custom error() function. So be sure to load it.
+    # Class property
+        modelling_data: dict. data for model training. data is split if necessary.
+            No data splitting for the "CV only" mode.
+            returns a dict object with 'training' and 'test' items
+    """
 
-#     def __init__(self, cwd, file,
-#                  outcome_var, annotation_vars, sample_id_var,
-#                  model_type, n_classes,
-#                  cv_only,
-#                  man_split, holdout_samples, training_percentage, random_state, verbose):
-#         """
-#         # Arguments
-#             cwd: str. working directory
-#             file: str. complete input file path. "args.file[0]" from argparser]
-#             outcome_var: str. variable nanme for outcome. Only one is accepted for this version. "args.outcome_var" from argparser
-#             annotation_vars: list of strings. Column names for the annotation variables in the input dataframe, EXCLUDING outcome variable.
-#                 "args.annotation_vars" from argparser
-#             sample_id_var: str. variable used to identify samples. "args.sample_id_var" from argparser
-#             model_type: str. model type, classification or regression
-#             n_classes: int. number of classes when model_type='classification'
-#             cv_only: bool. If to split data into training and holdout test sets. "args.cv_only" from argparser
-#             man_split: bool. If to use manual split or not. "args.man_split" from argparser
-#             holdout_samples: list of strings. sample IDs for holdout sample, when man_split=True. "args.holdout_samples" from argparser
-#             training_percentage: float, betwen 0 and 1. percentage for training data, when man_split=False. "args.training_percentage" from argparser
-#             random_state: int. random state
-#             verbose: bool. verbose. "args.verbose" from argparser
-#         # Public class attributes
-#             Below are attributes read from arguments
-#                 self.cwd
-#                 self.model_type
-#                 self.n_classes
-#                 self.file
-#                 self.outcome_var
-#                 self.annotation_vars
-#                 self.cv_only
-#                 self.holdout_samples
-#                 self.training_percentage
-#                 self.rand: int. random state
-#             self.y_var: single str list. variable nanme for outcome
-#             self.filename: str. input file name without extension
-#             self.raw: pandas dataframe. input data
-#             self.raw_working: pands dataframe. working input data
-#             self.complete_annot_vars: list of strings. column names for the annotation variables in the input dataframe, INDCLUDING outcome varaible
-#             self.n_features: int. number of features
-#             self.le: sklearn LabelEncoder for classification study
-#             self.label_mapping: dict. Class label mapping codes, when model_type='classification'
-#         # Private class attributes (excluding class properties)
-#             self._basename: str. complete file name (with extension), no path
-#             self._n_annot_col: int. number of annotation columns
-#         """
-#         # setup working director
-#         self.cwd = cwd
+    def __init__(self, cwd, file,
+                 outcome_var, annotation_vars, sample_id_var,
+                 model_type, n_classes,
+                 cv_only,
+                 man_split, holdout_samples, training_percentage, random_state, verbose):
+        """
+        # Arguments
+            cwd: str. working directory
+            file: str. complete input file path. "args.file[0]" from argparser]
+            outcome_var: str. variable nanme for outcome. Only one is accepted for this version. "args.outcome_var" from argparser
+            annotation_vars: list of strings. Column names for the annotation variables in the input dataframe, EXCLUDING outcome variable.
+                "args.annotation_vars" from argparser
+            sample_id_var: str. variable used to identify samples. "args.sample_id_var" from argparser
+            model_type: str. model type, classification or regression
+            n_classes: int. number of classes when model_type='classification'
+            cv_only: bool. If to split data into training and holdout test sets. "args.cv_only" from argparser
+            man_split: bool. If to use manual split or not. "args.man_split" from argparser
+            holdout_samples: list of strings. sample IDs for holdout sample, when man_split=True. "args.holdout_samples" from argparser
+            training_percentage: float, betwen 0 and 1. percentage for training data, when man_split=False. "args.training_percentage" from argparser
+            random_state: int. random state
+            verbose: bool. verbose. "args.verbose" from argparser
+        # Public class attributes
+            Below are attributes read from arguments
+                self.cwd
+                self.model_type
+                self.n_classes
+                self.file
+                self.outcome_var
+                self.annotation_vars
+                self.cv_only
+                self.holdout_samples
+                self.training_percentage
+                self.rand: int. random state
+            self.y_var: single str list. variable nanme for outcome
+            self.filename: str. input file name without extension
+            self.raw: pandas dataframe. input data
+            self.raw_working: pands dataframe. working input data
+            self.complete_annot_vars: list of strings. column names for the annotation variables in the input dataframe, INDCLUDING outcome varaible
+            self.n_features: int. number of features
+            self.le: sklearn LabelEncoder for classification study
+            self.label_mapping: dict. Class label mapping codes, when model_type='classification'
+        # Private class attributes (excluding class properties)
+            self._basename: str. complete file name (with extension), no path
+            self._n_annot_col: int. number of annotation columns
+        """
+        # setup working director
+        self.cwd = cwd
 
-#         # random state
-#         self.rand = random_state
-#         self.verbose = verbose
+        # random state
+        self.rand = random_state
+        self.verbose = verbose
 
-#         # load files
-#         self.model_type = model_type
-#         self.n_classes = n_classes
-#         # convert to a list for training_test_spliter_final() to use
-#         self.outcome_var = outcome_var
-#         self.annotation_vars = annotation_vars
-#         self.y_var = [self.outcome_var]
+        # load files
+        self.model_type = model_type
+        self.n_classes = n_classes
+        # convert to a list for training_test_spliter_final() to use
+        self.outcome_var = outcome_var
+        self.annotation_vars = annotation_vars
+        self.y_var = [self.outcome_var]
 
-#         # args.file is a list. so use [0] to grab the string
-#         self.file = os.path.join(self.cwd, file)
-#         self._basename = os.path.basename(file)
-#         self.filename,  self._name_ext = os.path.splitext(self._basename)[
-#             0], os.path.splitext(self._basename)[1]
+        # args.file is a list. so use [0] to grab the string
+        self.file = os.path.join(self.cwd, file)
+        self._basename = os.path.basename(file)
+        self.filename,  self._name_ext = os.path.splitext(self._basename)[
+            0], os.path.splitext(self._basename)[1]
 
-#         if self.verbose:
-#             print('Loading file: ', self._basename, '...', end=' ')
+        if self.verbose:
+            print('Loading file: ', self._basename, '...', end=' ')
 
-#         if self._name_ext != ".csv":
-#             error('The input file should be in csv format.',
-#                   'Please check.')
-#         elif not os.path.exists(self.file):
-#             error('The input file or directory does not exist.',
-#                   'Please check.')
-#         else:
-#             self.raw = pd.read_csv(self.file, engine='python')
-#             self.raw_working = self.raw.copy()  # value might be changed
-#             self.complete_annot_vars = self.annotation_vars + self.y_var
-#             self._n_annot_col = len(self.complete_annot_vars)
-#             self.n_features = int(
-#                 (self.raw_working.shape[1] - self._n_annot_col))  # pd.shape[1]: ncol
+        if self._name_ext != ".csv":
+            error('The input file should be in csv format.',
+                  'Please check.')
+        elif not os.path.exists(self.file):
+            error('The input file or directory does not exist.',
+                  'Please check.')
+        else:
+            self.raw = pd.read_csv(self.file, engine='python')
+            self.raw_working = self.raw.copy()  # value might be changed
+            self.complete_annot_vars = self.annotation_vars + self.y_var
+            self._n_annot_col = len(self.complete_annot_vars)
+            self.n_features = int(
+                (self.raw_working.shape[1] - self._n_annot_col))  # pd.shape[1]: ncol
 
-#             self.cv_only = cv_only
-#             self.sample_id_var = sample_id_var
-#             self.holdout_samples = holdout_samples
-#             self.training_percentage = training_percentage
+            self.cv_only = cv_only
+            self.sample_id_var = sample_id_var
+            self.holdout_samples = holdout_samples
+            self.training_percentage = training_percentage
 
-#         if self.verbose:
-#             print('done!')
+        if self.verbose:
+            print('done!')
 
-#         if self.model_type == 'classification':
-#             self.le = LabelEncoder()
-#             self.le.fit(self.raw_working[self.outcome_var])
-#             self.raw_working[self.outcome_var] = self.le.transform(
-#                 self.raw_working[self.outcome_var])
-#             self.label_mapping = dict(
-#                 zip(self.le.classes_, self.le.transform(self.le.classes_)))
-#             if self.verbose:
-#                 print('Class label encoding: ')
-#                 for i in self.label_mapping.items():
-#                     print('{}: {}'.format(i[0], i[1]))
+        if self.model_type == 'classification':
+            self.le = LabelEncoder()
+            self.le.fit(self.raw_working[self.outcome_var])
+            self.raw_working[self.outcome_var] = self.le.transform(
+                self.raw_working[self.outcome_var])
+            self.label_mapping = dict(
+                zip(self.le.classes_, self.le.transform(self.le.classes_)))
+            if self.verbose:
+                print('Class label encoding: ')
+                for i in self.label_mapping.items():
+                    print('{}: {}'.format(i[0], i[1]))
 
-#         # call setter here
-#         if self.verbose:
-#             print('Setting up modelling data...', end=' ')
-#         self.modelling_data = man_split
-#         if self.verbose:
-#             print('done!')
+        # call setter here
+        if self.verbose:
+            print('Setting up modelling data...', end=' ')
+        self.modelling_data = man_split
+        if self.verbose:
+            print('done!')
 
-#     @property
-#     def modelling_data(self):
-#         # print("called getter") # for debugging
-#         return self._modelling_data
+    @property
+    def modelling_data(self):
+        # print("called getter") # for debugging
+        return self._modelling_data
 
-#     @modelling_data.setter
-#     def modelling_data(self, man_split):
-#         """
-#         Private attributes for the property
-#             _m_data: dict. output dictionary
-#             _training: pandas dataframe. data for model training.
-#             _test: pandas dataframe. holdout test data. Only available without the "--cv_only" flag
-#         """
-#         # print("called setter") # for debugging
-#         if self.cv_only:  # only training is stored
-#             self._training, self._test = self.raw_working, None
-#         else:
-#             # training and holdout test data split
-#             if man_split:
-#                 # manual data split: the checks happen in the training_test_spliter_final() function
-#                 self._training, self._test, _, _ = training_test_spliter_final(data=self.raw_working, random_state=self.rand,
-#                                                                                man_split=man_split, man_split_colname=self.sample_id_var,
-#                                                                                man_split_testset_value=self.holdout_samples,
-#                                                                                x_standardization=False, y_min_max_scaling=False)
-#             else:
-#                 if self.model_type == 'classification':  # stratified
-#                     train_idx, test_idx = list(), list()
-#                     stf = StratifiedShuffleSplit(
-#                         n_splits=1, train_size=self.training_percentage, random_state=self.rand)
-#                     for train_index, test_index in stf.split(self.raw_working, self.raw_working[self.y_var]):
-#                         train_idx.append(train_index)
-#                         test_idx.append(test_index)
-#                     self._training, self._test = self.raw_working.iloc[train_idx[0],
-#                                                                        :].copy(), self.raw_working.iloc[test_idx[0], :].copy()
-#                 else:  # regression
-#                     self._training, self._test, _, _ = training_test_spliter_final(
-#                         data=self.raw_working, random_state=self.rand, man_split=man_split, training_percent=self.training_percentage,
-#                         x_standardization=False, y_min_max_scaling=False)  # data transformation will be doen during modeling
-#         self._modelling_data = {
-#             'training': self._training, 'test': self._test}
+    @modelling_data.setter
+    def modelling_data(self, man_split):
+        """
+        Private attributes for the property
+            _m_data: dict. output dictionary
+            _training: pandas dataframe. data for model training.
+            _test: pandas dataframe. holdout test data. Only available without the "--cv_only" flag
+        """
+        # print("called setter") # for debugging
+        if self.cv_only:  # only training is stored
+            self._training, self._test = self.raw_working, None
+        else:
+            # training and holdout test data split
+            if man_split:
+                # manual data split: the checks happen in the training_test_spliter_final() function
+                self._training, self._test, _, _ = training_test_spliter_final(data=self.raw_working, random_state=self.rand,
+                                                                               man_split=man_split, man_split_colname=self.sample_id_var,
+                                                                               man_split_testset_value=self.holdout_samples,
+                                                                               x_standardization=False, y_min_max_scaling=False)
+            else:
+                if self.model_type == 'classification':  # stratified
+                    train_idx, test_idx = list(), list()
+                    stf = StratifiedShuffleSplit(
+                        n_splits=1, train_size=self.training_percentage, random_state=self.rand)
+                    for train_index, test_index in stf.split(self.raw_working, self.raw_working[self.y_var]):
+                        train_idx.append(train_index)
+                        test_idx.append(test_index)
+                    self._training, self._test = self.raw_working.iloc[train_idx[0],
+                                                                       :].copy(), self.raw_working.iloc[test_idx[0], :].copy()
+                else:  # regression
+                    self._training, self._test, _, _ = training_test_spliter_final(
+                        data=self.raw_working, random_state=self.rand, man_split=man_split, training_percent=self.training_percentage,
+                        x_standardization=False, y_min_max_scaling=False)  # data transformation will be doen during modeling
+        self._modelling_data = {
+            'training': self._training, 'test': self._test}
 
 
 # ------ model evaluation when cv_only=True ------
