af3836fad9b761414b80658c3320de758bd8f2a3
Jing Zhang <jzhangc@gmail.com>
2021-07-25 18:56:17 -0400

    routine updates

--- testrealm_batchloader.py
+++ testrealm_batchloader.py
@@ -186,21 +186,6 @@ class CnnClassifier(Model):
         return Model(inputs=[x], outputs=self.call(x))
 
 
-# ------ functions ------
-# def show_final_history(history):
-#     fig, ax = plt.subplots(1, 2, figsize=(15, 5))
-#     ax[0].set_title('loss')
-#     ax[0].plot(history.epoch, history.history["loss"], label="Train loss")
-#     ax[0].plot(history.epoch, history.history["val_loss"],
-#                label="Validation loss")
-#     ax[1].set_title('acc')
-#     ax[1].plot(history.epoch, history.history["accuracy"], label="Train acc")
-#     ax[1].plot(history.epoch, history.history["val_accuracy"],
-#                label="Validation acc")
-#     ax[0].legend()
-#     ax[1].legend()
-
-
 # ------ data ------
 # -- batch loader data --
 tst_tf_dat = BatchMatrixLoader(filepath='./data/tf_data', target_file_ext='txt',
