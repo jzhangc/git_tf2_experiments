f911fffcad04be944259e6d512e5fc40acc08749
Jing Zhang <jzhangc@gmail.com>
2021-08-17 17:37:30 -0400

    routine updates

---
+++ testrealm_plot_functions.py
@@ -0,0 +1,217 @@
+"""
+this is test realm for plot functions with tf.keras models using the data from BatchDataLoader()
+
+Objectives:
+[ ] extract values from tf.dataset objects
+[ ] ROC-AUC
+    [ ] calculate AUC
+    [ ] construct simple ROC
+    [ ] multiple classes
+    [ ] from One-Hot back to labels (figure legends)
+[ ] Recall and precision curve
+[ ] F1 curve
+"""
+
+
+# ------ load modules ------
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import (BatchNormalization, Conv2D, Dense,
+                                     Flatten, Input, Layer, LeakyReLU,
+                                     MaxPooling2D, Reshape, UpSampling2D)
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import roc_auc_score, roc_curve
+
+from utils.dl_utils import BatchMatrixLoader
+from utils.plot_utils import epochsPlot
+from utils.other_utils import flatten
+
+# ------ TF device check ------
+tf.config.list_physical_devices()
+
+
+# ------ model ------
+class AutoEncoderDecoder(Model):
+    def __init__(self, initial_shape, bottleneck_dim):
+        super(AutoEncoderDecoder, self).__init__()
+        self.initial_shape = initial_shape
+        self.bottleneck_dim = bottleneck_dim
+        self.encoder = CNN2d_encoder(
+            initial_shape=self.initial_shape, bottleneck_dim=bottleneck_dim)
+        self.decoder = CNN2d_decoder(encoded_dim=bottleneck_dim)
+
+    def call(self, input):  # putting two models togeter
+        x = self.encoder(input)
+        z = self.decoder(x)
+        return z
+
+    def encode(self, x):
+        """
+        This method is used to encode data using the trained encoder
+        """
+        x = self.encoder(x)
+        return x
+
+    def decode(self, z):
+        z = self.decoder(z)
+        return z
+
+    def model(self):
+        """
+        This method enables correct model.summary() results:
+        model.model().summary()
+        """
+        x = Input(self.initial_shape)
+        return Model(inputs=[x], outputs=self.call(x))
+
+
+class CnnClassifier(Model):
+    def __init__(self, initial_shape, bottleneck_dim, outpout_n):
+        super(CnnClassifier, self).__init__()
+        self.initial_shape = initial_shape
+        self.bottleneck_dim = bottleneck_dim
+        # CNN encoding sub layers
+        self.conv2d_1 = Conv2D(16, (3, 3), activation='relu', kernel_regularizer=tf.keras.regularizers.l2(l2=0.01),
+                               padding='same', input_shape=initial_shape)  # output: 28, 28, 16
+        self.bn1 = BatchNormalization()
+        self.leakyr1 = LeakyReLU()
+        self.maxpooling_1 = MaxPooling2D((2, 2))  # output: 14, 14, 16
+        self.conv2d_2 = Conv2D(8, (3, 3), activation='relu',
+                               padding='same')  # output: 14, 14, 8
+        self.bn2 = BatchNormalization()
+        self.leakyr2 = LeakyReLU()
+        # self.maxpooling_2 = MaxPooling2D((2, 2))  # output: 7, 7, 8
+        self.maxpooling_2 = MaxPooling2D((5, 5))  # output: 9, 9, 8
+        self.fl = Flatten()  # 7*7*8=392
+        self.dense1 = Dense(bottleneck_dim, activation='relu',
+                            activity_regularizer=tf.keras.regularizers.l2(l2=0.01))
+        self.encoded = LeakyReLU()
+        self.dense2 = Dense(outpout_n, activation='softmax')
+
+    def call(self, input):
+        x = self.conv2d_1(input)
+        x = self.bn1(x)
+        x = self.leakyr1(x)
+        x = self.maxpooling_1(x)
+        x = self.conv2d_2(x)
+        x = self.bn2(x)
+        x = self.leakyr2(x)
+        x = self.maxpooling_2(x)
+        x = self.fl(x)
+        x = self.dense1(x)
+        x = self.encoded(x)
+        x = self.dense2(x)
+        return x
+
+    def model(self):
+        """
+        This method enables correct model.summary() results:
+        model.model().summary()
+        """
+        x = Input(self.initial_shape)
+        return Model(inputs=[x], outputs=self.call(x))
+
+    def predict_classes(self, x, batch_size=32, verbose=1):
+        """
+        Generate class predictions for the input samples
+        batch by batch.
+        # Arguments
+            x: input data, as a Numpy array or list of Numpy arrays
+                (if the model has multiple inputs).
+            batch_size: integer.
+            verbose: verbosity mode, 0 or 1.
+        # Returns
+            A numpy array of class predictions.
+        """
+        proba = self.predict(x, batch_size=batch_size, verbose=verbose)
+        if proba.shape[-1] > 1:
+            return to_categorical(proba.argmax(axis=1), proba.shape[-1])
+        else:
+            return (proba > 0.5).astype('int32')
+
+
+# ------ data ------
+# -- batch loader data --
+tst_tf_dat = BatchMatrixLoader(filepath='./data/tf_data', target_file_ext='txt',
+                               manual_labels=None, label_sep=None, model_type='classification',
+                               multilabel_classification=False, x_scaling='minmax', x_min_max_range=[0, 1], resmaple_method='random',
+                               training_percentage=0.8, verbose=False, random_state=1)
+
+# below: manual labels
+tst_tf_dat = BatchMatrixLoader(filepath='./data/tf_data', target_file_ext='txt',
+                               manual_labels='./data/tst_annot.csv',
+                               manual_labels_fileNameVar='filename', manual_labels_labelVar='label',
+                               label_sep=None, model_type='classification',
+                               multilabel_classification=False, x_scaling='minmax', x_min_max_range=[0, 1], resmaple_method='random',
+                               training_percentage=0.8, verbose=False, random_state=1)
+tst_tf_train, tst_tf_test = tst_tf_dat.generate_batched_data(batch_size=10)
+
+
+# ------ training ------
+# -- early stop and optimizer --
+earlystop = EarlyStopping(monitor='val_loss', patience=5)
+callbacks = [earlystop]
+optm = Adam(learning_rate=0.001)
+
+# -- model --
+tst_m = CnnClassifier(initial_shape=(90, 90, 1),
+                      bottleneck_dim=64, outpout_n=10)
+tst_m.model().summary()
+
+# -- training --
+tst_m.compile(optimizer=optm, loss="categorical_crossentropy",
+              metrics=['categorical_accuracy', tf.keras.metrics.Recall()])
+tst_m_history = tst_m.fit(tst_tf_train, epochs=80,
+                          callbacks=callbacks,
+                          validation_data=tst_tf_test)
+
+
+# ------ plot function test ------
+epochsPlot(model_history=tst_m_history,
+           accuracy_var='categorical_accuracy',
+           val_accuracy_var='val_categorical_accuracy')
+
+
+pred = tst_m.predict(tst_tf_test)
+pred_class = tst_m.predict_classes(tst_tf_test)
+pred[0]
+pred.shape
+tst_tf_test
+
+to_categorical(np.argmax(pred, axis=1), 10)
+
+
+for _, b in tst_tf_test:
+    print(b)
+    # break
+
+tst_t = np.ndarray((0, 10))
+for _, b in tst_tf_test:
+    # print(b.numpy())
+    bn = b.numpy()
+    # print(type(bn))
+    tst_t = np.concatenate((tst_t, bn), axis=0)
+tst_t.shape
+
+
+fpr, tpr, thresholds = roc_curve(tst_t[:, 0], pred[:, 0])
+auc_score = roc_auc_score(tst_t[:, 0], pred[:, 0])
+
+plt.plot(fpr, tpr)
+plt.axis([0, 1, 0, 1])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.show()
+
+
+def tstfoo(y, pred):
+    """ROC-AUC plot function"""
+
+    return None
